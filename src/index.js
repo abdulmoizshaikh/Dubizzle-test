f476dfd8ac8fe7fb34e1d4ab9c500f5b614f8997
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2021-03-08 18:49:23 +0500

    dotenv configures

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { debugContextDevtool } from "react-context-devtool";
 
+// configure dotenv
+require("dotenv").config();
 
 const container = document.getElementById("root");
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  container);
+  container
+);
 
-/**
- configure react context devtool for debugging state
- */
-let options = {
-  debugReducer: true, //by default true
-  debugContext: true, //by default true
-};
+// to run ContextDevtool only in development mode
+if (process.env.NODE_ENV === "development") {
+  /**
+   configure react context devtool for debugging state
+   */
+  let options = {
+    debugReducer: true, //by default true
+    debugContext: true, //by default true
+  };
 
-debugContextDevtool(container, options);
+  debugContextDevtool(container, options);
+}
